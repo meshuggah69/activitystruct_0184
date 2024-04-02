d8bd51465fe35d3c484f9fd9cb1193365c60c3ca
athar <athar@THUGSHAKERZ>
2024-04-02 10:24:58 +0700

    menambah int main

--- activitystruct_0184_3/activitystruct_0184_3.cpp
+++ activitystruct_0184_3/activitystruct_0184_3.cpp
@@ -11,3 +11,26 @@ struct mahasiswa {
 	DetailAlamat alamat;
 	int umur;
 };
+int main() {
+	mahasiswa mhs;
+	cout << "Nomor mahasiswa : ";
+	getline(cin, mhs.nim);
+	cout << "Nama Mahasiswa : ";
+	getline(cin, mhs.nama);
+
+	cout << "alamat mahasiswa : " << endl;
+	cout << "\t nama Desa : ";
+	cin >> mhs.alamat.desa;
+	cout << "\t Nama kota : ";
+	cin >> mhs.alamat.kota;
+	cout << "Umur mahasiswa : ";
+	cin >> mhs.umur;
+
+	cout << endl;
+	cout << "\n Nim :" << mhs.nim;
+	cout << "\n Nama : " << mhs.nama;
+	cout << "\n alamat : ";
+	cout << "\n \t Desa : " << mhs.alamat.desa;
+	cout << "\n \t Kota : " << mhs.alamat.kota;
+	cout << "\n Umur : " << mhs.umur;
+}
