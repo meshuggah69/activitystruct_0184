0c5917cf0c95bd7abfdb95b4aaa3d3d5a4ab249d
athar <athar@THUGSHAKERZ>
2024-04-02 10:32:22 +0700

    menambah for (int i = 0; i < 3; i++)

--- activitystruct_0184_4/activitystruct_0184_4.cpp
+++ activitystruct_0184_4/activitystruct_0184_4.cpp
@@ -14,29 +14,4 @@ struct mahasiswa {
 int main() {
 	mahasiswa mhs[3];
 	for (int i = 0; i < 3; i++) {
-		cout << "data ke-" << (i + 1) << ":" << endl;
-		cout << "Nomor mahasiswa : ";
-		cout << "Nama Mahasiswa : ";
-
-		cout << "alamat mahasiswa : " << endl;
-		cout << "\t nama Desa : ";
-		cin >> mhs[i].alamat.desa;
-		cout << "\t Nama kota : ";
-		cin >> mhs[i].alamat.kota;
-		cout << "Umur mahasiswa : ";
-		cin >> mhs[i].umur;
-		cin.ignore(1, '\n');
-		cout << endl;
-	}
-	for (int i = 0; i < 3; i++) {
-		cout << endl;
-		cout << "Data mahasiswa ke-" << (i = 1) << ":" << endl;
-		cout << "\n Nim :" << mhs[i].nim;
-		cout << "\n Nama : " << mhs[i].nama;
-		cout << "\n alamat : ";
-		cout << "\n \t Desa : " << mhs[i].alamat.desa;
-		cout << "\n \t Kota : " << mhs[i].alamat.kota;
-		cout << "\n Umur : " << mhs[i].umur;
-		cout << endl;
-	}
-}
+		
