fe9c148c9233c14f85fc9d0915e07e63ca4bcbbf
athar <athar@THUGSHAKERZ>
2024-04-02 10:35:18 +0700

    menambah cout umur mahasiswa

--- activitystruct_0184_4/activitystruct_0184_4.cpp
+++ activitystruct_0184_4/activitystruct_0184_4.cpp
@@ -23,7 +23,7 @@ int main() {
 		cin >> mhs[i].alamat.desa;
 		cout << "\t Nama kota : "; 
 		cin >> mhs[i].alamat.kota;
-		cout << "Umur mahasiswa : ";
+		cout << "Umur mahasiswa : "; 
 		cin >> mhs[i].umur;
 		cin.ignore(1, '\n');
 		cout << endl;
