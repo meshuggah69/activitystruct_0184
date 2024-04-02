d5b30a8b121433c4b1270fe8e959823538426792
athar <athar@THUGSHAKERZ>
2024-04-02 10:03:37 +0700

    membuat int main

--- activitystruct_0184/activitystruct_0184.cpp
+++ activitystruct_0184/activitystruct_0184.cpp
@@ -7,3 +7,21 @@ struct mahasiswa {
 	string alamat;
 	int umur;
 };
+
+int main() {
+	mahasiswa  mhs;
+	cout << "nomor Mahasiswa : ";
+	cin >> mhs.nim;
+	cout << "Nama Mahasiswa :";
+	cin >> mhs.nama;
+	cout << "alamat mahasiswa : ";
+	cin >> mhs.alamat;
+	cout << "Umur mahasiswa : ";
+	cin >> mhs.umur;
+
+	cout << endl;
+	cout << "\n Nim :" << mhs.nim;
+	cout << "\n Nama : " << mhs.nama;
+	cout << "\n alamat : " << mhs.alamat;
+	cout << "\n Umur : " << mhs.umur;
+}
